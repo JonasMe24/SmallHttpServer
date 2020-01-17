73e8d12c38c47d60523fa7de7583025a0b619b01
jonas <jonas@192.168.0.12>
2020-01-17 22:08:03 +0100

    initial

---
+++ small-http-server/src/main/java/de/meierj/small_http_server/App.java
@@ -0,0 +1,13 @@
+package de.meierj.small_http_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
